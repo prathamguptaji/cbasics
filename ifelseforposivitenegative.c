1729e7d0ac0dfe5675badc87a6951091e0b41cfa
Pratham Gupta <gpratham522@gmail.com>
2025-09-14 20:04:07 +0530

    Create ifelseforposivitenegative.c

---
+++ ifelseforposivitenegative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=10;
+    if(a>0)
+    {
+        printf("Positive number");
+    }
+    else
+    {
+        printf("Negative number");
+    }
+    return 0;
+}
